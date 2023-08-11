e137a21fbf8fc61128714dcd74a42a3ab5a00a56
exgoya <bam125@naver.com>
2023-08-11 18:01:28 +0900

    add with

--- Trc.py
+++ Trc.py
@@ -6,18 +6,26 @@ class Trc:
     def __init__(self, name):
       self.name = name
 
+    def __enter__(self):
+        print(self.name)
+        return self
+
     def search(self,dirname):
         with os.scandir(dirname) as scans:
             for scan in scans:
                 if self.name in scan.name:
                    self.filenames.append(scan.name)
 
+    def __exit__(self,exc_type,exe_val,exe_tb):
+       del self
+       print('with exit')
 
-d = Trc("system.trc")
-d.search("./lgtrc/G1N1")
+
+with Trc("system.trc") as d:
+  d.search("./lgtrc/G1N1")
 
 #for filename in d.filenames:
-print(d.filenames)
+  print(d.filenames)
 
-del d
+#del d
 #print(d.name)
