46f57d69b0d49a04891e0d1d2ce6570ded84230b
exgoya <bam125@naver.com>
2023-10-06 18:34:56 +0900

    change arg

--- Trc.py
+++ Trc.py
@@ -4,24 +4,24 @@ class Trc:
     filenames = []
 
     # 인스턴스 생성시 init
-    def __init__(self, name):
-      self.name = name
+    def __init__(self):
+        pass
  
     # with 호출시 반환될 객체
     def __enter__(self):
          return self
 
     # search 함수
-    def search(self,dirname):
+    def search(self,dirname,filename):
         with os.scandir(dirname) as scans:
             for scan in scans:
-                if self.name in scan.name:
+                if filename in scan.name:
                    self.filenames.append(scan.name)
 
     # with 구문을 빠져나가기 전 수행됨
     def __exit__(self,exc_type,exe_val,exe_tb):
        del self
 
-with Trc("system.trc") as d:
-  d.search("./lgtrc/G1N1")
+with Trc() as d:
+  d.search("./lgtrc/G1N1","system.trc")
   print(d.filenames)
